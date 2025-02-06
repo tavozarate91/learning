29a1b49eefe09ff124d3057bc4c756ed401501e4
Gustavo <tavozarate91@inst-builder-debian-11-build-build-70ptt.us-central1-a.c.gce-image-builder.internal>
2025-02-06 14:41:34 +0000

    update2

--- Calculator.java
+++ Calculator.java
@@ -9,4 +9,20 @@ public interface Calculator{
 
 
 //Implementation by Camilo
-//?
+//
+public Class Calculator{
+    public int result;
+    public int x;
+    public int y;
+
+    public Calculator(int var_x, int var_y){
+	    this.x = var_x;
+	    this.y = var_y;
+    }
+
+    public int sum(){
+	    return this.result = this.x + this.y; 
+
+    }
+
+}
