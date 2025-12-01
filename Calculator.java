16d15ccfcb58bd21ee26e588f440785ddba1e57d
gus <tavozarate91@gmail.com>
2025-12-01 04:41:24 +0000

    local-gus: testing done

--- Calculator.java
+++ Calculator.java
@@ -9,4 +9,11 @@ public interface Calculator{
 
 
 //Testing by Gustavo
-//Do it fast using chatgpt
+public class TestCalculator {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator(5, 7);
+        int sum = calc.sum();
+        System.out.println("Sum: " + sum);          // Expected: 12
+        System.out.println("Result field: " + calc.result); // Expected: 12
+    }
+}
