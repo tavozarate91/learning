84b0401d59d8f27a36c92cdd9a5972da15bf7da6
tavozarate91 <74512310+tavozarate91@users.noreply.github.com>
2025-12-01 04:26:02 +0100

    remote - tasks

--- Calculator.java
+++ Calculator.java
@@ -1,26 +1,10 @@
 //Interface Gustavo
 public interface Calculator{
     String calculate(String expression);
-
     int sum (a, b);
 }
 
 //Implementation by Camilo
-public Class Calculator{
-    public int result;
-    public int x;
-    public int y;
 
-    public Calculator(int var_x, int var_y){
-	    this.x = var_x;
-	    this.y = var_y;
-    }
-
-    public int sum(){
-	    return this.result = this.x + this.y; 
-
-    }
-
-}
 
 //Testing by Gustavo
