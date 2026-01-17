e43e9ee9abf7d1e21b75e7ac62d6779d127707d3
gus <tavozarate91@gmail.com>
2026-01-17 19:17:25 +0000

    c2

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,9 @@
 public interface Calculator{
     String calculate(String expression);
 }
+
+@Test
+void test_calculator(){
+    Calculator calc = ...;
+    assert(calc.calculate("1 + 1").equals("2"));
+}
